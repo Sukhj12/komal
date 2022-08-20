99199e63833b70618ecbb553239df31c591ac7e8
Sukhmanpreet <101450549+Sukhj12@users.noreply.github.com>
2022-08-20 11:50:39 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -362,8 +362,10 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     chat_id=query.from_user.id,
                     file_id=file_id,
                     caption=f_caption,
-                    protect_content=True if ident == "filep" else False 
-                )
+                    protect_content=True if ident == "filep" else False)
+          fek = await client.send_cached_media()
+                await asyncio.sleep()
+                await fek.delete()
                 await query.answer('Check PM, I have sent files in pm', show_alert=True)
         except UserIsBlocked:
             await query.answer('Unblock the bot mahn !', show_alert=True)
