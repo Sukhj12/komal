175ddd1529aa19fd4d3babe0599de8a6d6c17967
Sukhmanpreet <101450549+Sukhj12@users.noreply.github.com>
2022-08-20 15:35:40 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -244,7 +244,7 @@ async def start(client, message):
         caption=f_caption+f"\nThis File Will Be Deleted In {round(DELETE_TIMER/60)} minutes. So Forward To Your Saved Message.",
         protect_content=True if pre == 'filep' else False,
         )
-        await asyncio.sleep(300)
+        await asyncio.sleep(6)
         await dm.delete()    
         
 
